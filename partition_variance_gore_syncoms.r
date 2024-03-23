70d2625825731b2b0fe9e458c89ee795566eaa21
sur <surh@users.noreply.github.com>
2024-03-23 16:49:54 -0600

    Confirming that my code matches Villemereuil, but more general

--- partition_variance_gore_syncoms.r
+++ partition_variance_gore_syncoms.r
@@ -63,67 +63,63 @@ AIC(mp0, mp1, mp2, mc)
 BIC(mp0, mp1, mp2, mc)
 
 #' ## Extract params from polynomial models
-mp <- mp2
-
-#' Design matrix
-design_mat <- model.matrix(mp)
-
-#' Variance-covariance matrix
-varcov <- VarCorr(mp)[["Community"]]
-attr(varcov, "stddev") <- attr(varcov, "correlation") <- NULL
-
-# Average (fixed) effects
-out <- summary(mp)[["coefficients"]]
-coefs <- out[ , "Estimate"]
-se    <- out[ , "Std. Error"]
+partition_variance_polynomial <- function(mp,
+                                          pheno_name = "OD"){
+  #' Design matrix
+  design_mat <- model.matrix(mp)
   
-# Residual variance
-V_Res <- attr(VarCorr(mp), "sc")^2
-
-# Variance-covariance of environment
-varcov_design <- cov(design_mat)
+  #' Variance-covariance matrix
+  varcov <- VarCorr(mp)[["Community"]]
+  attr(varcov, "stddev") <- attr(varcov, "correlation") <- NULL
   
-# # Returning the list of parameters
-# tibble(a    = coefs[1],
-#        b    = coefs[2],
-#        c    = coefs[3],
-#        a_se = se[1],
-#        b_se = se[2],
-#        c_se = se[3],
-#        Va   = varcov[1, 1],
-#        Vb   = varcov[2, 2],
-#        Vc   = varcov[3, 3],
-#        Cab  = varcov[1, 2],
-#        Cac  = varcov[1, 3],
-#        Cbc  = varcov[2, 3],
-#        V_R   = V_R) 
+  # Average (fixed) effects
+  out <- summary(mp)[["coefficients"]]
+  coefs <- out[ , "Estimate"]
+  se    <- out[ , "Std. Error"]
+  
+  # Residual variance
+  V_Res <- attr(VarCorr(mp), "sc")^2
+  
+  # Variance-covariance of environment
+  varcov_design <- cov(design_mat)
+  
+  # For a polynomial model we have an unbiased estimator for V_plas
+  # \hat{V}_{Plas} = \bar\theta^TX\theta - Tr(S_\theta X)
+  V_Plas <- coefs %*% varcov_design %*% coefs - se %*% varcov_design %*% se
+  V_Plas <- as.numeric(V_Plas)
+  
+  #' For V_gen we have:
+  #' V_{Gen} = E_\epsilon(x^T\Theta x) = \bar{x}^T\Theta\bar{x} + Tr(\Theta X)
+  #' Is this equivalent?
+  V_Gen <- sum( diag( (1/nrow(design_mat)) * (t(design_mat) %*% design_mat) %*% varcov ) )
+  
+  
+  # V_Tot <- V_Plas + V_Gen + V_Res
+  V_Phen <- var(model.frame(mp)[,pheno_name])
+  
+  Pi <- (coefs^2 * diag(varcov_design) - se^2) / V_Plas
+  Gamma <- ( ( ( t(design_mat) %*% design_mat ) / nrow(design_mat) ) * varcov ) / V_Gen
+  
+  return(list(design_mat = design_mat,
+              varcov_design = varcov_design,
+              varcov = varcov,
+              coefs = coefs,
+              se = se,
+              V_Phen = V_Phen,
+              V_Tot = V_Plas + V_Gen + V_Res,
+              V_Plas = V_Plas,
+              V_Gen = V_Gen,
+              V_Res = V_Res,
+              Pi = Pi,
+              Gamma = Gamma))
+}
+
+partition_variance_polynomial(mp = mp0, pheno_name = "OD")
+partition_variance_polynomial(mp = mp1, pheno_name = "OD")
+partition_variance_polynomial(mp = mp2, pheno_name = "OD")
 
 
-#'  ## Variance partitioning
-#'  For a polynomial model we have an unbiased estimator for V_plas
-#'  \hat{V}_{Plas} = \bar\theta^TX\theta - Tr(S_\theta X)
-
-V_Plas <- coefs %*% varcov_design %*% coefs - se %*% varcov_design %*% se
-V_Plas <- as.numeric(V_Plas)
-
-#' For V_gen we have:
-#' V_{Gen} = E_\epsilon(x^T\Theta x) = \bar{x}^T\Theta\bar{x} + Tr(\Theta X)
-#' Is this equivalent?
-V_Gen <- sum( diag( (1/nrow(design_mat)) * (t(design_mat) %*% design_mat) %*% varcov ) )
-
-V_Tot <- V_Plas + V_Gen + V_Res
-V_Phen <- var(Dat$OD)
-V_Plas
-V_Gen
-V_Res
-V_Tot
-V_Phen
-
-
-Pi <- (coefs^2 * diag(varcov_design) - se^2) / V_Plas
-Pi
-Gamma <- 2 * varcov[1, 3] * mean(design_mat[,3]) / V_Gen
-Gamma
+#' ## Extract params from character state model
 
 
 
